1389134c4b22c558174e8d6e4a62680a34edf31e
jack <wrexthor@protonmail.com>
2017-04-28 16:15:07 +0200

    failing at regex

--- converter.py
+++ converter.py
@@ -30,7 +30,7 @@ def convert(input, decimals):
             # run to kg conversion
             result = convert_from_pounds(number, decimals)
         elif unit == 3:
-            result = None
+            result = convert_from_ounce(number, decimals)
         else:
             result = None
         click.echo(result)
@@ -40,17 +40,23 @@ def convert_from_kg(number, decimal):
     pound = round((number * 2.20462), decimal)
     gram = round((number * 1000), decimal)
     ounce = round((number * 35.2739619496), decimal)
-    click.echo('Multiplying {} with 2.20462'.format(number))  # debug
     number = round(number, decimal)
     click.echo('{}Kg equals {}pounds, {}grams or {}ounces'.format(number, pound, gram, ounce))
 
+
+
+def convert_from_ounce(number, decimal):
+    kg = round((number / 35.274), decimal)
+    pound = round((number * 0.0625), decimal)
+    gram = round((number / 0.035274), decimal)
+    number = round(number, decimal)
+    click.echo('{}ounce equals {}Kg, {}grams or {}pounds'.format(number, kg, gram, pound))
 
 
 def convert_from_pounds(number, decimal):
     kg = round((number / 2.20462), decimal)
     gram = round(((number / 2.20462) * 1000), decimal)
     ounce = round((number * 16.000), decimal)
-    click.echo('Dividing {} with 2.2046'.format(number))  # debug
     number = round(number, decimal)
     click.echo('{}pounds equals {}Kg, {}grams or {}ounces'.format(number, kg, gram, ounce))
 
@@ -58,12 +64,16 @@ def convert_from_pounds(number, decimal):
 
 def get_number(text):
     number = re.search(r'\d+', text).group()
-    #number = filter(str.isdigit, text)
-    click.echo('The number {} found in text'.format(number))
+    click.echo('The number {} found in text'.format(number))  # debug
     return float(number)
 
 
 def find_word(text, word):
+    # replace with an awesome regex to avoid getting weird matches for short abbrevations like L
+    # regex finding word trailing a space
+    #space = re.search('\d|(?<= )\w+\b', word)
+    # regex looking for digit or space before word, does not work AT ALL!
+    # result = re.search('\b|\d%s' % word, text)
     if word in text:
         return True
     else:
@@ -72,11 +82,17 @@ def find_word(text, word):
 
 def get_unit(text):
     mass = {'kg': 1, 'kilo': 1, 'kilogram': 1, 'kilogramme': 1, 'pound': 2, 'lb': 2, 'lbs': 2, "lb's": 2, 'ounce': 3, 'oz': 3}
-    length = {'m': 1, 'metre': 1, 'meter': 1, 'inch': 2, 'in': 2}
+    length = {'m': 4, 'metre': 4, 'meter': 4, 'inch': 5, 'in': 5, 'feet': 6, 'foot': 6, 'ft': 6, 'yard': 7, 'yd': 7, 'mile': 8, 'Km': 9, 'kilometer': 9}
+    volume = {'millilitre': 10, 'ml': 10, 'litre': 11, 'liter': 11, 'l': 11, 'pint': 12, 'pt': 12, 'gallon': 13, 'gal': 13}
 
-    for key, val in mass.items():
+    # merge dicts for iteration
+    units = mass.copy()
+    units.update(length)
+    units.update(volume)
+
+    for key, val in units.items():
         if find_word(text, key):
-            click.echo('The unit {} found in text'.format(key))
+            click.echo('The unit {} found in text'.format(key))  # debug
             return val
 
 
